a25cdaddf6d7b4ee51ec02c3132b6912d3fd8c9f
thehiddenrocky <akshenndragarg17@gmail.com>
2024-11-16 00:04:18 +0200

    Jupyer notebook

---
+++ config_template.py
@@ -0,0 +1,7 @@
+# config_template.py (share this with team)
+REDDIT_CONFIG = {
+    'client_id': 'YOUR_CLIENT_ID_HERE',
+    'client_secret': 'YOUR_CLIENT_SECRET_HERE',
+    'user_agent': 'APP_NAME/1.0 (by /u/USERNAME)',
+    'redirect_uri': 'YOUR_REDIRECT_URI'
+}
